88cb17383433d875dce568d2ffd176becb1f27bf
LauraBLei <laura.blei@outlook.com>
2025-07-17 22:29:58 +0200

    fix: error where logo loading in as light mode no matter what

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,36 +1,23 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import Link from "next/link";
 import React from "react";
-import useMounted from "src/hooks/useMounted";
 import { FillImage, ImageContainer } from "../shared/FillImage";
 
 const Logo: React.FC = () => {
-  const { resolvedTheme } = useTheme();
-  const isMounted = useMounted;
-
-  if (!isMounted) {
-    return (
-      <Link href="/">
-        <ImageContainer className="aspect-[5/2] w-[120px] p-2 flex justify-center items-center cursor-pointer">
-          <FillImage
-            className="object-contain"
-            src="/logo/light.png"
-            alt="Plateful Logo"
-            sizes="120px"
-          />
-        </ImageContainer>
-      </Link>
-    );
-  }
-
   return (
     <Link href="/">
       <ImageContainer className="aspect-[5/2] w-[120px] p-2 flex justify-center items-center cursor-pointer">
         <FillImage
-          className="object-contain"
-          src={resolvedTheme === "dark" ? "/logo/dark.png" : "/logo/light.png"}
+          className="object-contain block dark:hidden"
+          src="/logo/light.png"
+          alt="Plateful Logo"
+          sizes="120px"
+        />
+
+        <FillImage
+          className="object-contain hidden dark:block"
+          src="/logo/dark.png"
           alt="Plateful Logo"
           sizes="120px"
         />
