b29c3946cad5edcfb48eb64af008e787985930d0
LauraBLei <laura.blei@outlook.com>
2025-07-26 20:25:47 +0200

    removed follow sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 import { fetchRecentRecipes, fetchTimeRecipes } from "src/api/recipeActions";
 import { Homepage } from "src/components/pages/HomePage";
-import { getCurrentUser } from "src/helpers/getCurrentUser";
-import { Recipe } from "src/types/recipe";
-import { getRecipesFromFollowers } from "../server/supabase";
+// import { getCurrentUser } from "src/helpers/getCurrentUser";
+// import { Recipe } from "src/types/recipe";
+// import { getRecipesFromFollowers } from "../server/supabase";
 
 // Force dynamic rendering since we check authentication
 export const dynamic = "force-dynamic";
 
 const Home = async () => {
-  let followerRecipes: Recipe[] = [];
+  // let followerRecipes: Recipe[] = [];
 
-  const currentUser = await getCurrentUser();
+  // const currentUser = await getCurrentUser();
 
-  if (currentUser?.following && currentUser.following.length > 0) {
-    const randomFollower =
-      currentUser.following[
-        Math.floor(Math.random() * currentUser.following.length)
-      ];
-    followerRecipes = await getRecipesFromFollowers(randomFollower);
-  }
+  // if (currentUser?.following && currentUser.following.length > 0) {
+  //   const randomFollower =
+  //     currentUser.following[
+  //       Math.floor(Math.random() * currentUser.following.length)
+  //     ];
+  //   followerRecipes = await getRecipesFromFollowers(randomFollower);
+  // }
 
   const [recentRecipes, timeRecipes] = await Promise.all([
     fetchRecentRecipes(),
@@ -29,7 +29,7 @@ const Home = async () => {
     <Homepage
       recentRecipes={recentRecipes}
       timeRecipes={timeRecipes}
-      followerRecipes={followerRecipes}
+      // followerRecipes={followerRecipes}
     />
   );
 };
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -5,25 +5,25 @@ import { SectionComponent } from "../shared/SectionComponent";
 interface HomepageProps {
   recentRecipes: Recipe[];
   timeRecipes: Recipe[];
-  followerRecipes?: Recipe[];
+  // followerRecipes?: Recipe[];
 }
 
 export const Homepage = ({
   recentRecipes,
   timeRecipes,
-  followerRecipes,
-}: HomepageProps) => {
+}: // followerRecipes,
+HomepageProps) => {
   return (
     <div className="max-w-[1440px] mb-30 w-full px-2 font-primary flex flex-col gap-5">
       <WelcomeSection />
-      {followerRecipes && followerRecipes.length > 0 && (
-        // <SectionComponent
-        //   recipeList={followerRecipes}
-        //   sectionName={`Recent Recipes from ${
-        //     followerRecipes[0]?.owner?.name || "Unknown"
-        //   }`}
-        // />
-      )}
+      {/* {followerRecipes && followerRecipes.length > 0 && (
+        <SectionComponent
+          recipeList={followerRecipes}
+          sectionName={`Recent Recipes from ${
+            followerRecipes[0]?.owner?.name || "Unknown"
+          }`}
+        />
+      )} */}
       <SectionComponent recipeList={recentRecipes} sectionName={"Recent"} />
       <SectionComponent
         recipeList={timeRecipes}
