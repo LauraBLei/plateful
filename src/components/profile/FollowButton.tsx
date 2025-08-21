f1191a263340f13ce66ffa36f7e6ac29dcd1ae1e
LauraBLei <laura.blei@outlook.com>
2025-08-21 13:17:02 +0200

    fav and follow button now updates on click and not on reload

--- src/components/profile/FollowButton.tsx
+++ src/components/profile/FollowButton.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useAuth } from "src/providers/AuthProvider";
 import { UserProfile } from "src/types/user";
 import { updateUser } from "../../api/userActions";
